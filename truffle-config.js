27119a26094b311eeb206dc81027ff6fc225f2bf
Clark <clark@bytecache.io>
2020-11-25 10:05:40 -0800

    Added truffle/hdwallet-provider, rinkeby config to deploy to testnet

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,14 @@
 const path = require("path");
 
+
+
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const infuraURL = 'https://rinkeby.infura.io/v3/7291b19128f64740ac9da315b3e0bc91'
+const infuraKey = fs.readFileSync(".infuraProjectSecret").toString().trim();
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".metamaskWalletMnemonic").toString().trim();
+
 module.exports = {
     // See <http://truffleframework.com/docs/advanced/configuration>
     // to customize your Truffle configuration!
@@ -8,18 +17,27 @@ module.exports = {
     compilers: {
         solc: {
             version: "0.6.12",    // Fetch exact version from solc-bin (default: truffle's version)
-	    optimizer: {
-		enabled: true,
-		runs: 200,
-	    },
+            optimizer: {
+                enabled: true,
+                runs: 200,
+            },
         },
     },
 
     networks: {
+
         development: {
             host: "127.0.0.1",
             port: "8545",
             network_id: "*",
-        }
-    }
+            gas: 5500000,
+        },
+
+        //rinkeby: {
+        //    provider: () => new HDWalletProvider(mnemonic, infuraURL),
+        //    network_id: 4,       // Rinkeby's network id
+        //    gas: 5500000,
+        //},
+
+    },
 };
